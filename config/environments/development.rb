46776e63ca06fc68a5a48b406f52c8447187c4d8
Kolomoets.Oleg <KolomoetsOleg@yandex.ru>
2014-01-07 00:17:12 +0200

    rails_template: set email accounts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ SampleApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
