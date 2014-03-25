e49a33eb2b20484fb165eabd0546ddc0b00739d4
Kolomoets.Oleg <KolomoetsOleg@yandex.ru>
2014-03-25 02:17:10 +0200

    Stop on 9.3.4

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name                  "Kolomoets Oleg"
-    email                 "kolomoets.oleg@gmail.com"
-    password              "123456789"
-    password_confirmation "123456789"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
