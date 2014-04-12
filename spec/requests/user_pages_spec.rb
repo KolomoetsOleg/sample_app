1f1eb6e812b1bf4fe608167da03d969a98087fae
Kolomoets.Oleg <KolomoetsOleg@yandex.ru>
2014-04-12 13:19:34 +0300

    Finish chapter 9 (without home task)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "User pages" do
+
   subject { page }
 
   # Index page
@@ -16,7 +17,7 @@ describe "User pages" do
 
     describe "pagination" do
 
-      before(:all) { 30.times{ FactoryGirl.create(:user) } }
+      before(:all) { 50.times{ FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
 
       it { should have_selector('div.pagination') }
@@ -27,6 +28,33 @@ describe "User pages" do
         end
       end
     end
+
+    #  delete user for admin
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+
+        it "should be able to delete another user " do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+
+        it { should_not have_link('delete', href: user_path(admin)) }
+
+      end
+
+    end
+
   end
 
   # Sign Up pages
